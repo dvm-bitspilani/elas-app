b35651431dc0af985cf300575dd372954a22bf0a
Alvin Uday <udayalvin@gmail.com>
2022-04-09 12:24:33 +0530

    latest changes

---
+++ build/precache-manifest.ce1152396910aecb2f03d022e5781945.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c16f34752aa13ac8c4fd93d1459d017",
+    "url": "/index.html"
+  },
+  {
+    "revision": "97915bf1d0e7b0c4a976",
+    "url": "/static/css/main.b421054c.chunk.css"
+  },
+  {
+    "revision": "51eb1390e6153a1d98c5",
+    "url": "/static/js/2.6e7f67b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.6e7f67b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97915bf1d0e7b0c4a976",
+    "url": "/static/js/main.124e8a36.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  },
+  {
+    "revision": "4e74c9bbef68f658e623cedf33ff1f3a",
+    "url": "/static/media/Logo.4e74c9bb.jpeg"
+  },
+  {
+    "revision": "9a7c849b216648913f216d20a21e9aae",
+    "url": "/static/media/SF-Pro-Text-Semibold.9a7c849b.otf"
+  },
+  {
+    "revision": "85bd46c1cff02c1d8360cc714b8298fa",
+    "url": "/static/media/SFProText-Regular.85bd46c1.ttf"
+  },
+  {
+    "revision": "d3d97745d7631ad8f1a09915ffc1da65",
+    "url": "/static/media/background.d3d97745.png"
+  }
+]);
